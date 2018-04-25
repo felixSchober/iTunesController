e3d9005f8cc5ff85b34aacfcdc0d33ae016f1f99
Felix Schober <github@felix-schober.com>
2018-04-25 10:48:11 +0200

    change default namespace and fix route problem in CurrentTrackController

--- iTunesServer/Controllers/CurrentTrackController.cs
+++ iTunesServer/Controllers/CurrentTrackController.cs
@@ -1,26 +1,22 @@
-﻿using System.Drawing.Imaging;
-using System.IO;
-using System.Net;
-using System.Net.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Schober.Felix.ITunes.Controller;
 using Schober.Felix.ITunes.Controller.Model;
 
-namespace iTunesServer.Controllers
+namespace Schober.Felix.ITunes.Server.Controllers
 {
     [Route("api/[controller]")]
-    public class CurrentTrackController : Controller
+    public class CurrentTrackController : Microsoft.AspNetCore.Mvc.Controller
     {
-        //[HttpGet]
-        //public Track Get()
-        //{
-        //    var currentTrack = ITunesService.Instance.GetCurrenTrack();
-        //    return currentTrack;
-        //}
+        [HttpGet]
+        public Track Get()
+        {
+            var currentTrack = ITunesService.Instance.GetCurrenTrack();
+            return currentTrack;
+        }
 
         [HttpGet]
-        //[Route("api/[controller]/cover")]
-        public IActionResult Get()
+        [Route("cover")]
+        public IActionResult GetCover()
         {
             var track = ITunesService.Instance.GetCurrenTrack();
             var coverPath = track.GetPathToTrackArtwork();
@@ -28,10 +24,6 @@ namespace iTunesServer.Controllers
 
             var image = System.IO.File.OpenRead(coverPath);
             return File(image, "image/jpeg");
-
-
-
-
         }
     }
 }
--- iTunesServer/Program.cs
+++ iTunesServer/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using iTunesServer;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
-namespace iTunesServer
+namespace Schober.Felix.ITunes.Server
 {
     public class Program
     {
